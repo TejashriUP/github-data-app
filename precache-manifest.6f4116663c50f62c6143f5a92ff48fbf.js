760e472c849c4970f816fc4762f200a310400c9d
Tejashri Panchbhai <tejashripanchbhai9@gmail.com>
2020-10-24 22:31:23 +0530

    Updates

---
+++ precache-manifest.6f4116663c50f62c6143f5a92ff48fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c739d90ce70d880c1ced0b482c81f6b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc7f1c01ca82733a7ea",
+    "url": "/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "0fa866484d1b75cffef8",
+    "url": "/static/js/2.18bb2fbd.chunk.js"
+  },
+  {
+    "revision": "1cd5940c0c7670126e009a5aded697d7",
+    "url": "/static/js/2.18bb2fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc7f1c01ca82733a7ea",
+    "url": "/static/js/main.740d60e6.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  }
+]);
